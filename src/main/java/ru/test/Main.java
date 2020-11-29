cfd291ade60fa696a574e32c717e700b9a8944b2
Sergey <csn11@yandex.ru>
2020-11-29 20:29:29 +0300

    Добавить 1

--- src/main/java/ru/test/Main.java
+++ src/main/java/ru/test/Main.java
@@ -3,7 +3,8 @@ package ru.test;
 public class Main {
     public static void main (String[] args){
         System.out.println("Привет привет");
-        System.out.println("Привет ");
+        System.out.println("Привет страна");
+
 
     }
 }
