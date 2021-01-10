2f22b3c00150fad9689f4a3461c708f21a83d8db
ChukinSergey <73614975+ChukinSergey@users.noreply.github.com>
2021-01-10 21:46:14 +0300

    Update Main.java

--- src/main/java/ru/test/Main.java
+++ src/main/java/ru/test/Main.java
@@ -3,7 +3,7 @@ package ru.test;
 public class Main {
     public static void main (String[] args){
         System.out.println("Привет привет");
-        System.out.println("Привет ");
+        System.out.println("Привет выв");
 
     }
 }
